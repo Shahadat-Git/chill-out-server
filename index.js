5f05c05ce11567bce795cfa16987b430710762b1
Shahadat-Git <noyon560@gmail.com>
2023-05-24 22:23:05 +0600

    chefs data added on chefs info route

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const port = 5000;
+const chefsInfo = require('./data/chefs-info.json');
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('server running');
-})
+});
+
+app.get('/chefs-info', (req, res) => {
+    res.send(chefsInfo);
+});
 
 app.listen(port, () => {
-    console.log('server running on this port : ', port)
-})
+    console.log('server running on this port : ', port);
+});
