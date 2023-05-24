7f3d953b4b9499f0c9a38aedca8aaa351901ec5e
Shahadat-Git <noyon560@gmail.com>
2023-05-24 22:19:07 +0600

    successfully created express app with port

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const express = require('express');
 const port = 5000;
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('server running');
+})
+
+app.listen(port, () => {
+    console.log('server running on this port : ', port)
+})
