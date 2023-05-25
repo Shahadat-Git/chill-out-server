ea24b33ed207f477a3d36d6a093a4bd96d2ae23c
Shahadat-Git <noyon560@gmail.com>
2023-05-25 06:35:31 +0600

    cors adde

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const port = 5000;
 const chefsInfo = require('./data/chefs-info.json');
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('server running');
